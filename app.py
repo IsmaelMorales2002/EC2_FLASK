eabe6d8d7ffc94b4ea5e424932f2b2df29a5409d
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-04-14 18:46:43 -0600

    Actualizacion Usuario

--- app.py
+++ app.py
@@ -15,5 +15,16 @@ def upload_file():
     print('EXITO')
     return "Archivo de Usuario Subido Exitosamente", 200
 
+@app.route('/usuario/',methods=['POST'])
+def update_usuario():
+    if 'file' not in request.files:
+        return 'No se encontro el archivo en la solicitud', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No se selecciono ningún archivo',400
+    file.save(os.path.join(UPLOAD_FOLDER,file.filename))
+    print('EXITO')
+    return 'Archivo de Usuario Subido Exitosamente',200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
